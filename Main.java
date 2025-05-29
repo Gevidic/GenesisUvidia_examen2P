57ba60066aba09a84b6b74e781e6c67316078150
gevidic <genesisuvidia5@gmail.com>
2025-05-29 17:16:09 -0500

    commitseis

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
 public class Main {
     //main
-
+//Hola puedes creaR un programa en java que cumpla con este diagrama UML,
+        GU_Cuenta
         float saldo;
         int numeroDepositos =0;
         int numeroRetiros =0;
         float tasaAnual;
+GU_Cuenta(float saldo,float tasaAnual)
+depositar(float cantidad):void
+        retirar(float cantidad):void
 
+GU_CuentaAhorros
 
+                activa boolean
+        GU_CuentaAhorros(float saldo, float tasa)
+                depositar(float cantidad): void
+        retirar(float cantidad):void
 
-
+        Y finalmente imprime un menu con todo esto en el main
 
 }
